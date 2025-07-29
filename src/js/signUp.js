114a24938534cbf9ec90b1aa018f8e9d81186c38
ZnDream <gladysneo925@gmail.com>
2025-07-29 02:02:27 +0100

    Update heading in signUp.html and add form validation in signUp.js

--- src/js/signUp.js
+++ src/js/signUp.js
@@ -2,12 +2,24 @@ const supabaseUrl = "https://nwnszbtxvpbxkspcooyi.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53bnN6YnR4dnBieGtzcGNvb3lpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzODQwNDMsImV4cCI6MjA2ODk2MDA0M30.RykiTWCvNpDUe_EiPbo3_lduV5gXxO2e5b6tTPDClIE";
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
-document.getElementById('signForm').addEventListener('submit', async(e) =>{
-    e.preventDefault();
+document.getElementById('signForm').addEventListener('submit', async (e) => {
+  e.preventDefault();
 
-const firstName = document.getElementById('firstName').value.trim();
-const surname = document.getElementById('surname').value.trim();
-const email =  document.getElementById('email').value.trim();
+  const firstName = document.getElementById('firstName').value.trim();
+  const surname = document.getElementById('surname').value.trim();
+  const email = document.getElementById('email').value.trim();
+
+  // Validate email format
+  const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  if (!isValidEmail) {
+    alert('Please enter a valid email address.');
+    return;
+  }
+  // Check if fields are empty
+  if (!firstName || !surname || !email) {
+    alert('Please fill in all fields.');
+    return;
+  }
 
   const { data, error } = await supabase
     .from('mailing_list')
